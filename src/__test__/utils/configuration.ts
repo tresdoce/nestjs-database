0644391cc9c2177fe1aa7c14031b5d7cad3ccf08
Mex Delgado <mdelgado@tresdoce.com.ar>
2021-12-01 22:53:10 -0300

    chore(workflow/develop): add env secrets from repo

--- src/__test__/utils/configuration.ts
+++ src/__test__/utils/configuration.ts
@@ -4,13 +4,12 @@ export default registerAs('config', () => {
   return {
     database: {
       mongo: {
-        connectionName: 'tresdoce-local',
-        connection: 'mongodb',
-        user: encodeURIComponent('mdelgado'),
-        password: encodeURIComponent('34716927'),
-        host: '127.0.0.1',
-        port: parseInt('27017', 10),
-        dbName: 'tresdoce',
+        connection: process.env.MONGO_CONNECTION || 'mongodb',
+        user: encodeURIComponent(process.env.MONGO_USER),
+        password: encodeURIComponent(process.env.MONGO_PASSWORD),
+        host: process.env.MONGO_HOST,
+        port: parseInt(process.env.MONGO_PORT, 10),
+        dbName: process.env.MONGO_DB_NAME,
       },
     },
   };
--- src/mongo/mongo.module.ts
+++ src/mongo/mongo.module.ts
@@ -1,16 +1,7 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-
-interface MongoDatabaseConfig {
-  connectionName: string;
-  connection: string;
-  user: string;
-  password: string;
-  host: string;
-  port: number;
-  dbName: string;
-}
+import { MongoDatabaseConfig } from './types';
 
 @Global()
 @Module({
@@ -18,12 +9,9 @@ interface MongoDatabaseConfig {
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
-        const { connectionName, connection, user, password, host, port, dbName } =
+        const { connection, user, password, host, port, dbName } =
           configService.get<MongoDatabaseConfig>('config.database.mongo');
         return {
-          connectionName,
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
           uri: `${connection}://${user ? user : ''}${password ? `:${password}@` : ''}${host}${
             port ? `:${port}` : ''
           }/${dbName}`,
